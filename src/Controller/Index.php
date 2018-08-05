65d7c6f1201bfc50e435de1f4d4e108bee24276c
biq <biq@176-114-2-20.thehost.com.ua>
2018-08-05 13:01:49 +0300

    fix index controller

--- src/Controller/Index.php
+++ src/Controller/Index.php
@@ -7,9 +7,6 @@ class Index extends \FrontController {
 	public function actionIndex() {
 		
 		$model = new \Model\Test('test');
-		
-		echo $m->ggg();
-		
 		$this->content = \View::factory('Index');
 	}
 	
